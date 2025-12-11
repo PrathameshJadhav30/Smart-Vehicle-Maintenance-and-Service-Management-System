1c23aa950909ddb4100dc2bc56983b6e3458a639
Prathamesh <prathameshaj2004@gmail.com>
2025-12-11 20:16:11 +0530

    SVMMS-Testing Setup

---
+++ backend/jest.config.js
@@ -0,0 +1,20 @@
+export default {
+  testEnvironment: 'node',
+  collectCoverageFrom: [
+    'src/**/*.js',
+    '!src/server.js',
+    '!src/config/**',
+    '!src/migrations/**',
+    '!src/seeders/**'
+  ],
+  testMatch: [
+    '**/__tests__/**/*.js',
+    '**/?(*.)+(spec|test).js'
+  ],
+  transform: {
+    '^.+\\.js$': 'babel-jest'
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!axios)/'
+  ]
+};
